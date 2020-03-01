f173f5e6dcc3782e02fc4c13f190cccdccb04a41
Philipp Seifried <mail@philippseifried.com>
2020-03-01 16:41:12 +0100

    Added ability to swizzle axes / channels.

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ A simple Blender 2.8x addon that copies a selected object's normals to its verte
 Based on the stackexchange solution here: https://blender.stackexchange.com/questions/32584/set-vertex-normals-to-vertex-color-in-python
 
 ## Installation
-Navigate to Edit -> Preferences -> Addons -> Install. Select the .zip file or the unzipped __init__.py file.
+Navigate to Edit -> Preferences -> Addons -> Install. Select the .zip file or the unzipped normals_to_vertex_colors.py file.
 
 ## How To Use
-In Vertex Paint mode, select "Paint -> Vertex Color from Normals" in the menu.
+In Vertex Paint mode, select "Paint -> Vertex Color from Normals" in the menu. Depending on your engine, you'll want to change which axis is mapped to which rgb channel. For Unity, the parameters for FBX exported using the default settings are -X, +Y, +Z. For FBX exported using "Experimental Apply Transform", the settings are -X, +Z, -Y.
--- __init__.py
+++ __init__.py
@@ -19,80 +19,11 @@
 bl_info = {
     "name": "Normals To Vertex Colors",
     "author": "Philipp Seifried",
-    "version": (0, 1, 0),
+    "version": (0, 1, 1),
     "blender": (2, 80, 0),
     "location": "View3D > Paint > Vertex Color from Normals",
     "description": "Bakes selected object's normals to active vertex colors. Based on https://blender.stackexchange.com/questions/32584/set-vertex-normals-to-vertex-color-in-python",
     "category": "Paint",
 }
 
-import bpy
-from mathutils import Vector
-from bpy.props import (
-    EnumProperty,
-)
-
-
-class NormalsToVertexColors(bpy.types.Operator):
-    """Bakes selected object's normals to active vertex colors."""
-    bl_idname = "paint.normals_to_vcol"
-    bl_label = "Vertex Color from Normals"
-    bl_options = {'REGISTER', 'UNDO'}
-
-    space_items = (
-        ('WORLD', "World", "Normals are encoded in world space"),
-        ('LOCAL', "Local", "Normals are encoded in local space"),
-    )
-    space: EnumProperty(
-        name="Space",
-        items=space_items,
-        default='WORLD',
-    )
-
-    @classmethod
-    def poll(cls, context):
-        if (context.mode != 'PAINT_VERTEX'):
-            return False
-        
-        return True
-
-    def execute(self, context):
-        current_obj = bpy.context.active_object 
-        mesh = current_obj.data
-        if mesh.vertex_colors:
-            vcol_layer = mesh.vertex_colors.active
-        else:
-            vcol_layer = mesh.vertex_colors.new()
-
-        for loop_index, loop in enumerate(mesh.loops):
-                loop_vert_index = loop.vertex_index
-                normal = mesh.vertices[loop_vert_index].normal
-                if (self.space == 'LOCAL'):
-                    normal = current_obj.matrix_world.to_3x3() @ normal
-                    normal.normalize()
-                color = ( (normal * 0.5) + Vector((0.5,) * 3) )
-                color.resize_4d()
-                vcol_layer.data[loop_index].color = color
-
-        mesh.vertex_colors.active = vcol_layer
-        mesh.update()
-
-        return {'FINISHED'}
-
-
-def menu_func(self, context):
-    self.layout.operator(NormalsToVertexColors.bl_idname)
-
-
-def register():
-    bpy.utils.register_class(NormalsToVertexColors)
-    bpy.types.VIEW3D_MT_paint_vertex.append(menu_func)
-
-
-def unregister():
-    bpy.utils.unregister_class(NormalsToVertexColors)
-    bpy.types.VIEW3D_MT_mesh_add.remove(menu_func)
-
-
-if __name__ == "__main__":
-    register()
+from .normals_to_vertex_colors import *
